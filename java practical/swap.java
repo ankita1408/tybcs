169066509a314d0633e0a07b7f21eb34ca9ee20e
ankita1408 <50322298+ankita1408@users.noreply.github.com>
2020-09-07 18:20:37 +0530

    Create swap.java

---
+++ java practical/swap.java
@@ -0,0 +1,17 @@
+package tybcs;
+
+public class swap
+{
+  public static void main(String arg[])
+  { int a=10, b=20;
+     a=a+b;
+     b=a-b;
+     a=a-b;
+     
+     System.out.println("a:"+a);
+     System.out.println("b:"+b);
+  }
+  
+  
+
+}
