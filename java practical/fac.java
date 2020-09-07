434047f06025cdbe6c83bdfc0c40f336b7d23f52
ankita1408 <50322298+ankita1408@users.noreply.github.com>
2020-09-07 18:18:29 +0530

    Create fac.java

---
+++ java practical/fac.java
@@ -0,0 +1,14 @@
+package tybcs;
+
+public class fac 
+{
+	public static void main(String args[])
+	{
+		int no=5;int fac=1;
+		for(int i=1;i<=no;i++)
+		{
+			fac=fac*i;
+		}
+		System.out.println("fact"+fac);
+	}
+}
