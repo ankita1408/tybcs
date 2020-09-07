d35c1fe8a82c80a957fad1c3c24638769ed3f725
ankita1408 <50322298+ankita1408@users.noreply.github.com>
2020-09-07 18:18:14 +0530

    Create cp.java

---
+++ java practical/cp.java
@@ -0,0 +1,17 @@
+package tybcs;
+
+public class cp 
+{
+	public static void main(String args[])
+	{
+		int[] a= {11,22,33,44,55};
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]+" ");
+			
+		}
+		System.out.println("");
+	}
+	
+
+}
