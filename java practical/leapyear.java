7f8ba0149cdcc1a2a23bc1e9fb751d6c79c1ca9b
ankita1408 <50322298+ankita1408@users.noreply.github.com>
2020-09-07 18:18:58 +0530

    Create leapyear.java

---
+++ java practical/leapyear.java
@@ -0,0 +1,36 @@
+package tybcs;
+
+public class leapyear
+{
+	public static void main(String args[]) 
+	
+	{
+		int year=2011;
+		{
+	      if (year%4==0)
+	         {  
+		        if (year%100==0)
+		           {
+			           if(year%400==0)
+		                     {
+			                  System.out.println("leap year");
+		                      }
+		       
+		               else
+		               { System.out.println("not a leap year");
+		           }
+		           }
+	     else
+	       {
+		      System.out.println("leap year");
+	        }
+	         }
+	else
+		        { System.out.println("not a leap yeaer");
+	    }
+	    }
+	}
+		
+}	
+	
+
